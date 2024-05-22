83f1459cee10ac7f2997657dddb42083e83d6b07
XIZHOU <51404971+yangkun1994@users.noreply.github.com>
2024-05-23 01:27:30 +0800

    手动分饱

--- 03_rollup_使用插件/02_rollup_手动代码分割/rollup.config.js
+++ 03_rollup_使用插件/02_rollup_手动代码分割/rollup.config.js
@@ -1,5 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 
+// 在 01_rollup_plugins_nodeResolve 中将所有的第三方的代码全部打包进一个文件中，如果项目很多依赖，那么这个文件会很大，因此我们使用 rollup 进行手动分包
 const buildIndexOptions = {
     input: 'src/index.js',
     output: {
@@ -8,7 +9,10 @@ const buildIndexOptions = {
         // 控制入口文件的输出名称，默认是 "[name].js"
         entryFileNames: '[name].[hash].js',
         // 控制文件输出的名称，默认是 "[name]-[hash].js"
-        chunkFileNames: '[name].[hash].js'
+        chunkFileNames: '[name].[hash].js',
+        manualChunks: {
+            'lodash-es': ["lodash-es"]
+        }    
     },
     plugins: [
         // 这个插件可以将第三方库打包进我们最终的文件；需要先去掉 external 配置
