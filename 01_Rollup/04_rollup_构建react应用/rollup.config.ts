4a04278bef998c2a26f67a0ea740536a4ca73e44
XIZHOU <51404971+yangkun1994@users.noreply.github.com>
2024-06-16 23:03:25 +0800

    rollup react配置完整

--- 01_Rollup/04_rollup_构建react应用/rollup.config.ts
+++ 01_Rollup/04_rollup_构建react应用/rollup.config.ts
@@ -13,6 +13,10 @@ import image from '@rollup/plugin-image'
 import alias from '@rollup/plugin-alias'
 // 用来在 ESM 环境下处理绝对路径问题
 import { fileURLToPath } from 'node:url'
+// 代码压缩
+import terser from '@rollup/plugin-terser'
+// 打包分析
+import { visualizer } from 'rollup-plugin-visualizer'
 
 console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 
@@ -21,8 +25,42 @@ const config: RollupOptions = {
     output: {
         dir: './public/dist',
         format: 'esm',
-        sourcemap: true
+        sourcemap: true,
+        entryFileNames: '[name]-[hash].js',
+        // 动态加载分包
+        chunkFileNames: 'chunk/chunk-[name].[hash].js',
+        // 手动分包，把 react 相关的全部打包到一个文件中去
+        // manualChunks: {
+        //     react: ['react', 'react-dom'] 
+        // },
+        // terser 插件可以放在 output 配置里面，也可以放在 plugins 的配置中
+        plugins: [
+            terser()
+        ],
+        // 如果排除一些包不打包到最终的结果中，可以在打包最终的 html 中加入 CDN 链接
+        /**
+         * 目的: globals 字段用于指定模块在全局作用域中的变量名。
+         * 作用: 当 Rollup 打包时，如果发现代码中引用了 react 或 react-dom 模块，它将使用指定的全局变量名 React 和 ReactDOM 替代这些模块的导入。
+         * 示例应用: 假设你在项目中使用 import React from 'react';，在打包时 Rollup 将会把这个语句转换为使用全局变量 React，而不是生成一个本地的 React 变量。
+         */
+        globals: {
+            "react": "React", 
+            "react-dom": "ReactDOM"
+        },
+        /**
+         * 目的: paths 字段用于配置模块的路径映射。
+         * 作用: 当 Rollup 在打包时遇到 react 或 react-dom 的引用时，它将会根据这些映射将实际的模块路径替换为指定的 URL 地址。
+         * 示例应用: 这里的配置表明，当 Rollup 遇到 import React from 'react'; 或 import ReactDOM from 'react-dom'; 时，
+         * 它将从 https://cdn.jsdelivr.net/npm/react@18.3.1/+esm 和 https://cdn.jsdelivr.net/npm/react-dom@18.3.1/+esm 
+         * 这两个 URL 地址加载对应的模块，而不是从本地文件系统或其他默认位置加载。
+         */
+        paths: {
+            "react": "https://cdn.jsdelivr.net/npm/react@18.3.1/+esm",
+            "react-dom": "https://cdn.jsdelivr.net/npm/react-dom@18.3.1/+esm"
+        }
     },
+    // 指定不需要打包的文件, 如果有这个配置那么必须关闭 output 中的 manualChunks 配置
+    external: ['react', 'react-dom'],
     plugins: [
         nodeResolve(),
         commonjs(),
@@ -65,7 +103,9 @@ const config: RollupOptions = {
             template: './public/index.html',
             target: './public/dist/index.html',
             attrs: ['type="module"']
-        })
+        }),
+        // 会在根目录生成一个 stats.html 文件
+        visualizer()
     ]
 }
 
--- 01_Rollup/04_rollup_构建react应用/src/App.tsx
+++ 01_Rollup/04_rollup_构建react应用/src/App.tsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, lazy, Suspense } from 'react'
 
 import styles from '@/styles/app.module.scss'
 import Radio from '@/assets/radio-button-unchecked-svgrepo-com.svg'
 import yangjian from '@/assets/yangjian.png'
 import shihao from '@/assets/shihao.jpeg'
+import Loading from '@/components/Loading'
+
+const About = lazy(() => delay(import('@/components/About.tsx'), 2000))
 
 export default function App() {
     const [count, setCount] = useState(0)
+    const [visible, setVisible] = useState(false)
     return (
         <div>
             <h1>Hello dsadas</h1>
@@ -16,6 +20,24 @@ export default function App() {
             <img src={Radio} className={styles.img} alt="" />
             <img src={yangjian} className={styles.img} alt="" />
             <img src={shihao} className={styles.img} alt="" />
+            <br />
+            <button
+                className={styles.button}
+                onClick={() => setVisible(!visible)}
+            >show about</button>
+
+            {
+                visible && <Suspense fallback={<Loading />}>
+                    <About />
+                </Suspense>
+            }
         </div>
     )
+}
+
+// 延迟函数
+async function delay(promise: Promise<any>, delay: number) {
+    return new Promise(resolve => {
+        setTimeout(resolve, delay)
+    }).then(() => promise)
 }
